2caf6faeb414b93c28cd5067f66a16b88902839b
sam43 <scode43@gmail.com>
2019-07-03 01:18:47 +0600

    testing databinding

--- app/src/main/java/com/example/mvvmapp/view/activities/TestActivity.kt
+++ app/src/main/java/com/example/mvvmapp/view/activities/TestActivity.kt
@@ -3,6 +3,7 @@ package com.example.mvvmapp.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.mvvmapp.R
 import com.example.mvvmapp.viewModel.TestActivityVM
@@ -10,6 +11,7 @@ import com.sam43.customtextview.LoggingClass
 import kotlinx.android.synthetic.main.activity_test_activity.*
 import org.jetbrains.anko.startActivity
 import androidx.lifecycle.Observer
+import com.example.mvvmapp.databinding.ActivityTestActivityBinding
 import com.example.mvvmapp.models.Demo
 
 
@@ -23,10 +25,13 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         testVM = ViewModelProviders.of(this@TestActivity).get(TestActivityVM::class.java)
         amount = intent.getIntExtra("amount", 500)
-        setContentView(R.layout.activity_test_activity)
+        val binding : ActivityTestActivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_test_activity)
+        binding.name = "Your name"
+        binding.lastName = "Your last name"
         button.setOnClickListener {
             //startActivity<ListActivity>()
-            observeViewModel(testVM)
+           observeViewModel(testVM)
         }
     }
 
@@ -43,7 +48,7 @@ class TestActivity : AppCompatActivity() {
                 if (data != null) {
                     //…
                     //projectAdapter.setProjectList(projects)
-                    tvResponse.text = data.toString()
+                    //tvResponse.text = data.toString()
                     Log.d("Demo", "value: ${data.uData?.get(0)?.type}")
                 } else {
                     Log.d("Demo", "value: is getting null")
